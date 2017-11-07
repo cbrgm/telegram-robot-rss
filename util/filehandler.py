5ca46de2d9dab9e147bb91a80d50c2e251a6f2d1
Christian <chris@cbrgm.de>
2017-11-07 15:40:12 +0100

    Changed filehandler to be non static

--- util/filehandler.py
+++ util/filehandler.py
@@ -1,64 +1,57 @@
 import os
 import json
-from threading import Lock
 
 
 class FileHandler(object):
 
-    # staticpath reference
-    base_path = os.path.abspath(os.path.dirname(__file__))
+    def __init__(self, relative_root_path):
+        self.base_path = os.path.abspath(
+            os.path.dirname(__file__)) + "/" + relative_root_path + "/"
 
-    @staticmethod
-    def load_json(path):
+    def load_json(self, path):
         """Loads a json file and returns the content as a dictionary"""
-        filepath = os.path.join(FileHandler.base_path, '..', path)
+        filepath = os.path.join(self.base_path, path)
 
         with open(filepath) as jsonfile:
             data = json.load(jsonfile)
         return data
 
-    @staticmethod
-    def save_json(data, path):
+    def save_json(self, data, path):
         """Stores string json data to a json file at given path"""
-        filepath = os.path.join(FileHandler.base_path, '..', path)
+        filepath = os.path.join(self.base_path, path)
 
         with open(filepath, 'w+') as outfile:
             json.dump(data, outfile)
 
-    @staticmethod
-    def load_file(path):
+    def load_file(self, path):
         """Loads a file and returns the content as string"""
-        filepath = os.path.join(FileHandler.base_path, '..', path)
+        filepath = os.path.join(self.base_path, path)
 
         with open(filepath, "r") as file:
             data = file.read()
         return data
 
-    @staticmethod
-    def save_file(data, path):
+    def save_file(self, data, path):
         """Stores string data to a file at given path"""
-        filepath = os.path.join(FileHandler.base_path, '..', path)
+        filepath = os.path.join(self.base_path, path)
 
         with open(filepath, "w+") as file:
             data = file.write(str(data))
         return data
 
-    @staticmethod
-    def object2json(object, path):
+    def object2json(self, object, path):
         """Stores object values to a json file at given path"""
-        filepath = os.path.join(FileHandler.base_path, '..', path)
+        filepath = os.path.join(self.base_path, path)
 
         with open(filepath, 'w+') as outfile:
             json.dump(object.__dict__, outfile)
 
-    @staticmethod
-    def file_exists(path):
+    def file_exists(self, path):
         """Checks wether the given file exists or not"""
-        filepath = os.path.join(FileHandler.base_path, '..', path)
+        filepath = os.path.join(self.base_path, path)
         return os.path.exists(filepath)
 
-    @staticmethod
-    def get_files_in_dir(path):
+    def get_files_in_dir(self, path):
         """Returns a list containing all filenames in a given directory"""
-        filepath = os.path.join(FileHandler.base_path, '..', path)
+        filepath = os.path.join(self.base_path, path)
         return os.listdir(filepath)
