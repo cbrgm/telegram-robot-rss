91b07c7415d5422941996e4a28c815e8f2a88e29
Christian <chris@cbrgm.de>
2017-11-08 23:13:49 +0100

    Changed Dockerfile, added config init script for Docker

---
+++ .docker/initconfig.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+
+def main():
+    # Create Config
+    telegram_bot_token = os.environ["BOT_TOKEN"]
+    update_interval = os.environ["UPDATE_INTERVAL"]
+
+    config = {}
+    config["telegram_token"] = telegram_bot_token
+    config["update_interval"] = update_interval
+
+    base_path = os.path.abspath(os.path.dirname(__file__), "..")
+    filepath = os.path.join(base_path, "resources/credentials.json")
+
+    with open(filepath, 'w+') as outfile:
+        json.dump(config, outfile)
+
+
+if __name__ == '__main__':
+    main()
--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ WORKDIR /workspace
 COPY . /workspace
 
 # Environment Variables for future use
-ENV BOT_TOKEN
-ENV UPDATE_INTERVAL=300
+ENV BOT_TOKEN telegram_bot_token
+ENV UPDATE_INTERVAL 300
 
 RUN mkdir /workspace/resources/userdata
 RUN pip install -r requirements.txt
-CMD python rssbot.py
+CMD python .docker/initconfig.py && python robotrss.py
