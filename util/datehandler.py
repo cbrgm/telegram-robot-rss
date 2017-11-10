08a7461798603b7673d9dbbbc0680bbe8586a185
Christian <chris@cbrgm.de>
2017-11-10 02:31:06 +0100

    Refactored datehandler, removed bug not parsing naive datetimes

--- util/datehandler.py
+++ util/datehandler.py
@@ -1,15 +1,29 @@
 import pytz
 import datetime
+from dateutil import parser
 
 
 class DateHandler:
 
     @staticmethod
     def get_datetime_now():
+
+        # Strip seconds from datetime
         datestring = str(
             datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
-        unaware_dat = datetime.datetime.utcnow().strptime(datestring, "%Y-%m-%d %H:%M:%S")
-        localtz = pytz.timezone('UTC')
-        aware_est = localtz.localize(unaware_dat)
-        ber = pytz.timezone("Europe/Berlin")
-        return aware_est.astimezone(ber)
+        naive_date = datetime.datetime.utcnow().strptime(datestring, "%Y-%m-%d %H:%M:%S")
+
+        # Make datetime aware of timezone
+        aware_date = pytz.utc.localize(naive_date)
+        result = aware_date.astimezone(pytz.timezone("Europe/Berlin"))
+        return result
+
+    @staticmethod
+    def parse_datetime(datetime):
+        result = parser.parse(datetime)
+
+        if result.tzinfo is None:
+            aware_date = pytz.utc.localize(result)
+            result = aware_date.astimezone(pytz.timezone("Europe/Berlin"))
+
+        return result
